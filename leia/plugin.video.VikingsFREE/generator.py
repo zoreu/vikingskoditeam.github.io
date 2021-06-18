53f1a5dfb11fbb236bef3dc19d04994e3f78b36e
VikingsKodiTeam <74507453+vikingskoditeam@users.noreply.github.com>
2021-06-18 18:41:49 -0300

    update VikingsFree Leia

--- leia/plugin.video.VikingsFREE/generator.py
+++ leia/plugin.video.VikingsFREE/generator.py
@@ -1,23 +1,4 @@
 # -*- coding: utf-8 -*-
-
-'''
-    Genesis Add-on
-    Copyright (C) 2015 lambda
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
-
 import urllib,urllib2,urlparse,re,os,sys,xbmc,xbmcgui,xbmcaddon,xbmcvfs
 
 try:
@@ -28,7 +9,6 @@ try:
     import json
 except:
     import simplejson as json
-
 
 class get(object):
     def __init__(self, url):
@@ -1999,6 +1979,4 @@ class zettahost:
             url = js().worker(result)
             return url
         except:
-            return
-
-
+            return		
