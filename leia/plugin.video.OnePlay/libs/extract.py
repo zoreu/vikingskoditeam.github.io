b2f6961f2750d61747a0d81db32af71f666ecb20
vikingskoditeam <vikingsrepositorio@gmail.com>
2022-08-17 14:17:53 -0300

    Update Oneplay and Add KodiLoucos

--- leia/plugin.video.OnePlay/libs/extract.py
+++ leia/plugin.video.OnePlay/libs/extract.py
@@ -1,25 +1,27 @@
-#import zfile as zipfile
-import zipfile
-#from zipfile import ZipFile
+import six
+if six.PY3:
+    import zipfile
+else:
+    from libs.zipfile import zipfile
+    
 
 def all(_in, _out, dp=None):
     if dp:
-        return allWithProgress(_in, _out, dp)
-    return allNoProgress(_in, _out)
+        allWithProgress(_in, _out, dp)
+    else:
+        allNoProgress(_in, _out)
 
 def allNoProgress(_in, _out):
     try:
         zin = zipfile.ZipFile(_in, 'r')
         zin.extractall(_out)
     except Exception as e:
-        print (str(e))
-        return False
-    return True
+        pass
 
 def allWithProgress(_in, _out, dp):
     zin = zipfile.ZipFile(_in,  'r')
     nFiles = float(len(zin.infolist()))
-    count  = 0
+    count = 0
     try:
         for item in zin.infolist():
             count += 1
@@ -27,8 +29,4 @@ def allWithProgress(_in, _out, dp):
             dp.update(int(update))
             zin.extract(item, _out)
     except Exception as e:
-        print (str(e))
-        return False
-
-    return True
-#checkintegrity13122019
+        pass
